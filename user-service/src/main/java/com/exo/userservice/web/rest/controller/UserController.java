e71e8b72111c4a475c32c89bffe0b71e2ab62f50
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-04-01 10:23:28 +0700

    commit

--- user-service/src/main/java/com/exo/userservice/web/rest/controller/UserController.java
+++ user-service/src/main/java/com/exo/userservice/web/rest/controller/UserController.java
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @GetMapping("/list")
     public String getUsers() {
-        return "Danh sách users từ User Service";
+        List<String> users = List.of("Truong","ND Kien","Minh David");
+        return users.toString();
     }
 }
