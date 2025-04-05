c7fe5a9ccb7abc7d06c03fcd937aa38da9c16aa3
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-04-05 08:18:01 +0700

    commit 2

---
+++ all_service/service_user/src/main/java/com/exo/service_user/config/aop/logging/LoggingAspect.java
@@ -0,0 +1,34 @@
+package com.exo.service_user.config.aop.logging;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+@Aspect
+public class LoggingAspect {
+    private Logger logger(JoinPoint joinPoint) {
+        return LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
+    }
+
+    @Pointcut("execution(* com.exo.service_user.service.*.*(..)) || execution(* com.exo.service_user.repository.*.*(..))")
+    public void logMethods() {}
+
+    @Before("logMethods()")
+    public void logBefore(JoinPoint joinPoint) {
+        logger(joinPoint).info("Method {} is about to execute with arguments: {}",
+                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("logMethods()")
+    public void logAfter(JoinPoint joinPoint) {
+        logger(joinPoint).info("Method {} has executed", joinPoint.getSignature().getName());
+    }
+    @AfterThrowing(pointcut = "logMethods()", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        logger(joinPoint).error("Method {} threw an exception: {}", joinPoint.getSignature().getName(), exception.getMessage());
+    }
+
+
+}
---
+++ all_service/service_user/src/main/java/com/exo/service_user/web/rest/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.exo.service_user.web.rest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @GetMapping("/list")
+    public String getUsers() {
+        List<String> users = List.of("Truong","ND Kien","Minh David");
+        return users.toString();
+    }
+}
