8dc669c7c8d738871f0b2286755f1c427f0dcb96
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-04-05 08:36:18 +0700

    commit

--- all_service/service_user/src/main/java/com/exo/service_user/web/rest/controller/UserController.java
+++ all_service/service_user/src/main/java/com/exo/service_user/web/rest/controller/UserController.java
@@ -12,9 +12,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    Logger logger = LoggerFactory.getLogger(UserController.class);
     @GetMapping("/list")
     public String getUsers() {
         List<String> users = List.of("Truong","ND Kien","Minh David");
+        logger.info(users.toString());
         return users.toString();
     }
 }
